0f96b503b9795a502fdb93cbd76dad31b60161a8
chaitanyavankhande <66583050+chaitanyavankhande@users.noreply.github.com>
2023-06-04 19:45:01 +0530

    Create 1.1 Set Matrix Zeros.cpp

---
+++ 1.1 Set Matrix Zeros.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+void setZeros(vector<vector<int>> &matrix)
+{
+	// Write your code here.
+	int n = matrix.size(), m = matrix[0].size();
+	vector<int> rows(n, -1), cols(m, -1);
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<m; j++) {
+			if(matrix[i][j] == 0) {
+				rows[i] = 0;
+				cols[j] = 0;
+			}
+		}
+	}
+
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<m; j++) {
+			if(rows[i] == 0 || cols[j] == 0) {
+				matrix[i][j] = 0;
+			}
+		}
+	}
+	
+}
